f658c3cd9e0e0b22c6255fb8a3545c45eb54a4e9
nkrgangavaram <reddynithin33@gmail.com>
2025-06-07 08:13:21 -0700

    added static files rendering of frontend

--- Backend/server.js
+++ Backend/server.js
@@ -12,8 +12,12 @@ const { validateJWTToken } = require("./middleware/authorizationMiddleware");
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
 const mongoSanitize = require("express-mongo-sanitize");
+const path = require("path");
 
 connectDB();
+
+const clientBuildPath = path.join(__dirname,"../FrontEnd/dist");
+app.use(express.static(clientBuildPath));
 
 const apiLimiter = rateLimit({
   windowMS: 15 * 60 * 1000,
